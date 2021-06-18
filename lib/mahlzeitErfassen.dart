b725c5c5aec043e344f281793d8698ba95f42662
Thoughtness <davidkeller9200@gmail.com>
2021-06-18 20:46:50 +0200

    changed to table

--- lib/mahlzeitErfassen.dart
+++ lib/mahlzeitErfassen.dart
@@ -1,33 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:inner_peace/navigationMenu.dart';
 
-class mahlzeitErfassen extends StatelessWidget{
+class mahlzeitErfassen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
       backgroundColor: Colors.teal[100],
-    endDrawer: menu(),
-    appBar: AppBar(
-      title: Text('Mahlzeit erfassen'),
-      backgroundColor: Colors.cyanAccent,
-    ),
-    body: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(10),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(20),
-            width: 100,
-            child: const Text("Mahlzeit:"),
-            color: Colors.red[100],
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: const TextField(),
-          ),
-        ],
-    )
-  );
+      endDrawer: menu(),
+      appBar: AppBar(
+        title: Text('Mahlzeit erfassen'),
+        backgroundColor: Colors.cyanAccent,
+      ),
+      body: Table(border: TableBorder.all(),
+          columnWidths: const <int, TableColumnWidth>{
+            0: IntrinsicColumnWidth(),
+            1: FlexColumnWidth(),
+            2: FixedColumnWidth(50),
+          },
+          //defaultVerticalAlignment: TableCellVerticalAlignment.top,
+          children: <TableRow>[
+            TableRow(children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(20),
+                child: const Text("Mahlzeit:"),
+              ),
+              TableCell(
+                verticalAlignment: TableCellVerticalAlignment.top,
+                child: Container(
+                  height: 102,
+                  color: Colors.red,
+              ),
+              )]),
+          ]));
 }
