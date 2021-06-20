e52d024fd4a23ae0bd7c2b3c6f497a11877a9df0
Thoughtness <davidkeller9200@gmail.com>
2021-06-20 14:09:42 +0200

    save before implementing idea

--- lib/symptomeErfassenBody.dart
+++ lib/symptomeErfassenBody.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
-class symptomeErfassenBody extends StatelessWidget{
+class symptomeErfassenBody extends StatefulWidget{
+  @override
+  _symptomeErfassenBody createState(){
+    return _symptomeErfassenBody();
+  }
+}
 
+
+class _symptomeErfassenBody extends State{
+  @override
+  Widget build(BuildContext context) {
+    double value = 10.0;
+    return MaterialApp(
+        home: Column(
+          children: <Widget>[
+          Slider.adaptive(
+            value: value,
+            min: 1,
+            max: 10,
+            divisions: 10,
+            onChanged: (changedValue){
+              setState(() => value = changedValue);
+            },
+          ),
+          ],
+        ),
+    );
+  }
 }
