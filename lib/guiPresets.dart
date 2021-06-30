2c436c74e9bdb9b4588e34831fda6634727a54af
Thoughtness <davidkeller9200@gmail.com>
2021-06-30 17:43:26 +0200

    saving for the 3rd time

--- lib/guiPresets.dart
+++ lib/guiPresets.dart
@@ -1,162 +1,162 @@
-import 'package:flutter/material.dart';
-import 'package:inner_peace/symptomeErfassen.dart';
-import 'package:inner_peace/main.dart';
-
-class guiMahlzeitErfassen extends StatelessWidget {
-  String titel = "";
-  String zutaten = "";
-  String symptome = "";
-
-  guiMahlzeitErfassen(String titel, String zutaten, String symptome) {
-    this.titel = titel;
-    this.zutaten = zutaten;
-    this.symptome = symptome;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          const SizedBox(height: 10),
-          Flexible(
-            child: Row(
-              children: <Widget>[
-                const SizedBox(width: 10),
-                Flexible(
-                  flex: 20,
-                  child: Container(
-                      decoration: myBoxDecoration(),
-                      width:
-                      10000,
-                      //gross machen das die Breite genug gross ist damit flex greift
-                      height: 100,
-                      child: insertTitle(text: titel)),
-                ),
-                const SizedBox(width: 10),
-                Flexible(
-                  flex: 60,
-                  child: Container(
-                    height: 49,
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: ' Gericht hier benennen'),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-              ],
-            ),
-          ),
-          const SizedBox(height: 10),
-          Flexible(
-            child: Row(
-              children: <Widget>[
-                const SizedBox(width: 10),
-                Flexible(
-                  flex: 2,
-                  child: Container(
-                      decoration: myBoxDecoration(),
-                      width:
-                      10000,
-                      //gross machen das die Breite genug gross ist damit flex greift
-                      height: 49,
-                      child: insertTitle(text: zutaten)),
-                ),
-                const SizedBox(width: 10),
-                Flexible(
-                  flex: 6,
-                  child: Container(
-                    height: 49,
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: ' Zutaten mit Komma trennen'),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-              ],
-            ),
-          ),
-          const SizedBox(width: 10),
-          Flexible(
-            child: Row(
-              children: <Widget>[
-                const SizedBox(width: 10),
-                Flexible(
-                  flex: 20,
-                  child: Column(
-                    children: <Widget>[
-                      buttonBasic(
-                        text: 'Mahlzeit speichern',
-                        onClick: () =>
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                //toDo save inputs
-                                builder: (context) => symptomeErfassen(),
-                              ),
-                            ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(width: 10),
-          Flexible(
-            flex: 20,
-            child: Column(
-              children: <Widget>[
-                buttonBasic(
-                  text: 'Mahlzeit speichern',
-                  onClick: () =>
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          //toDo change to Hauptmenü and save inputs
-                          builder: (context) => symptomeErfassen(),
-                        ),
-                      ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(width: 10),
-        ],
-      ),
-    );
-  }
-
-  Widget insertTitle({
-    required String text,
-  }) {
-    return ListTile(
-      title: Text(text),
-    );
-  }
-
-  Widget buttonBasic({
-    required VoidCallback onClick,
-    required text,
-  }) {
-    return ElevatedButton(
-      onPressed: onClick,
-      child: Text(text),
-      style: TextButton.styleFrom(
-        backgroundColor: Colors.cyanAccent,
-        //minimumSize: Size(200, 45),
-        primary: Colors.black,
-      ),
-    );
-  }
-
-  BoxDecoration myBoxDecoration() {
-    return BoxDecoration(
-      border: Border.all(width: 1.0),
-      borderRadius: BorderRadius.all(Radius.circular(5.0) //
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:inner_peace/symptomeErfassen.dart';
+// import 'package:inner_peace/main.dart';
+//
+// class guiMahlzeitErfassen extends StatelessWidget {
+//   String titel = "";
+//   String zutaten = "";
+//   String symptome = "";
+//
+//   guiMahlzeitErfassen(String titel, String zutaten, String symptome) {
+//     this.titel = titel;
+//     this.zutaten = zutaten;
+//     this.symptome = symptome;
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Column(
+//         children: <Widget>[
+//           const SizedBox(height: 10),
+//           Flexible(
+//             child: Row(
+//               children: <Widget>[
+//                 const SizedBox(width: 10),
+//                 Flexible(
+//                   flex: 20,
+//                   child: Container(
+//                       decoration: myBoxDecoration(),
+//                       width:
+//                       10000,
+//                       //gross machen das die Breite genug gross ist damit flex greift
+//                       height: 100,
+//                       child: insertTitle(text: titel)),
+//                 ),
+//                 const SizedBox(width: 10),
+//                 Flexible(
+//                   flex: 60,
+//                   child: Container(
+//                     height: 49,
+//                     child: TextField(
+//                       decoration: InputDecoration(
+//                           border: OutlineInputBorder(),
+//                           hintText: ' Gericht hier benennen'),
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(width: 10),
+//               ],
+//             ),
+//           ),
+//           const SizedBox(height: 10),
+//           Flexible(
+//             child: Row(
+//               children: <Widget>[
+//                 const SizedBox(width: 10),
+//                 Flexible(
+//                   flex: 2,
+//                   child: Container(
+//                       decoration: myBoxDecoration(),
+//                       width:
+//                       10000,
+//                       //gross machen das die Breite genug gross ist damit flex greift
+//                       height: 49,
+//                       child: insertTitle(text: zutaten)),
+//                 ),
+//                 const SizedBox(width: 10),
+//                 Flexible(
+//                   flex: 6,
+//                   child: Container(
+//                     height: 49,
+//                     child: TextField(
+//                       decoration: InputDecoration(
+//                           border: OutlineInputBorder(),
+//                           hintText: ' Zutaten mit Komma trennen'),
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(width: 10),
+//               ],
+//             ),
+//           ),
+//           const SizedBox(width: 10),
+//           Flexible(
+//             child: Row(
+//               children: <Widget>[
+//                 const SizedBox(width: 10),
+//                 Flexible(
+//                   flex: 20,
+//                   child: Column(
+//                     children: <Widget>[
+//                       buttonBasic(
+//                         text: 'Mahlzeit speichern',
+//                         onClick: () =>
+//                             Navigator.of(context).push(
+//                               MaterialPageRoute(
+//                                 //toDo save inputs
+//                                 builder: (context) => symptomeErfassen(),
+//                               ),
+//                             ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           const SizedBox(width: 10),
+//           Flexible(
+//             flex: 20,
+//             child: Column(
+//               children: <Widget>[
+//                 buttonBasic(
+//                   text: 'Mahlzeit speichern',
+//                   onClick: () =>
+//                       Navigator.of(context).push(
+//                         MaterialPageRoute(
+//                           //toDo change to Hauptmenü and save inputs
+//                           builder: (context) => symptomeErfassen(),
+//                         ),
+//                       ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           const SizedBox(width: 10),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   Widget insertTitle({
+//     required String text,
+//   }) {
+//     return ListTile(
+//       title: Text(text),
+//     );
+//   }
+//
+//   Widget buttonBasic({
+//     required VoidCallback onClick,
+//     required text,
+//   }) {
+//     return ElevatedButton(
+//       onPressed: onClick,
+//       child: Text(text),
+//       style: TextButton.styleFrom(
+//         backgroundColor: Colors.cyanAccent,
+//         //minimumSize: Size(200, 45),
+//         primary: Colors.black,
+//       ),
+//     );
+//   }
+//
+//   BoxDecoration myBoxDecoration() {
+//     return BoxDecoration(
+//       border: Border.all(width: 1.0),
+//       borderRadius: BorderRadius.all(Radius.circular(5.0) //
+//       ),
+//     );
+//   }
+// }
--- lib/mealData.dart
+++ lib/mealData.dart
@@ -1,25 +1,9 @@
-final String tableNotes = 'notes';
-
-class NoteFields{
-  static final List<String> values = [
-    /// Add all fields
-    id, meal, ingredients, symptomTotal, generalWellbeing, cramps, flatulence, bowel
-  ];
-
-  static final String id = '_id';
-  static final String meal = '_meal';
-  static final String ingredients = '_ingredients';
-  static final String symptomTotal = '_symptomTotal';
-  static final String generalWellbeing = 'generalWellbeing';
-  static final String cramps = 'cramps';
-  static final String flatulence = 'flatulence';
-  static final String bowel = 'bowel';
-}
 // ignore: camel_case_types
 class mealData{
   int? id;
   String meal;
-  List<String> ingredients;
+  String ingredients;
+  //List<String> ingredients;
   double? symptomTotal;
   double? generalWellbeing;
   double? cramps;
@@ -37,47 +21,32 @@ class mealData{
     this.bowel,
   });
 
-  mealData copy({
-    int? id,
-    String? meal,
-    List<String>? ingredients,
-    double? symptomTotal,
-    double? generalWellbeing,
-    double? cramps,
-    double? flatulence,
-    double? bowel,
-  }) =>
-      mealData(
-        id: id ?? this.id,
-        meal: meal ?? this.meal,
-        ingredients: ingredients ?? this.ingredients,
-        symptomTotal: symptomTotal ?? this.symptomTotal,
-        generalWellbeing: generalWellbeing ?? this.generalWellbeing,
-        cramps: cramps ?? this.cramps,
-        flatulence: flatulence ?? this.flatulence,
-        bowel: bowel ?? this.bowel,
-      );
+  // String get _meal => meal;
 
-  static mealData fromJson(Map<String, Object?> json) => mealData(
-    id: json[NoteFields.id] as int?,
-    meal: json[NoteFields.meal] as String,
-    ingredients: json[NoteFields.ingredients] as List<String>,
-    symptomTotal: json[NoteFields.symptomTotal] as double?,
-    generalWellbeing: json[NoteFields.generalWellbeing] as double?,
-    cramps: json[NoteFields.cramps] as double?,
-    flatulence:[NoteFields.flatulence] as double?,
-    bowel:json[NoteFields.bowel] as double?,
-  );
+  Map<String, dynamic> toMap() {
+    return {
+      'id' : id,
+      'meal': meal,
+      'ingredients': ingredients,
+      'generalWellbeing': generalWellbeing,
+      'cramps': cramps,
+      'flatulence': flatulence,
+      'bowel': bowel,
+    };
+  }
 
-  Map<String, Object?> toJson() => {
-    NoteFields.id: id,
-    NoteFields.meal: meal,
-    NoteFields.ingredients: ingredients,
-    NoteFields.symptomTotal: symptomTotal,
-    NoteFields.generalWellbeing: generalWellbeing,
-    NoteFields.cramps: cramps,
-    NoteFields.flatulence: flatulence,
-    NoteFields.bowel: bowel,
-  };
+// mealData.mapToObject(Map<String, dynamic> map){
+//   this.meal = map['meal'];
+//   this.ingredients = map['ingredients'];
+//   this.symptomTotal = map['symptomTotal'];
+//   this.generalWellbeing = map['generalWellbeing'];
+//   this.cramps = map['cramps'];
+//   this.flatulence = map['flatulence'];
+//   this.bowel = map['bowel'];
+// }
 
+// @override
+// String toString() {
+//   return 'Mahlzeit{id: $id, meal: $meal, ingredients: $ingredients. symptomTotal: $symptomTotal, generalWellbeing: $generalWellbeing, cramps: $cramps, flatulence: $flatulence, bowel: $bowel}';
+// }
 }
